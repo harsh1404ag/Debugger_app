d9b1dbe9041e29c59d88871200fd41a180dd188b
Harsh Agnihotri <harshagnihotri14@gmail.com>
2025-07-12 16:21:48 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,4 +3,8 @@ import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+export function logout() {
+  localStorage.clear()
+  window.location.href = "/"
+}  
 }
