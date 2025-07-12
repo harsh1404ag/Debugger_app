552aab4fa34267f61cf14612c5b564b02799dca0
Harsh Agnihotri <harshagnihotri14@gmail.com>
2025-07-12 16:26:33 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,4 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
 export function logout() {
   localStorage.clear()
   window.location.href = "/"
-}  
 }
